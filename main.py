97465d38ecd3c0d8a3daf384cd797104899ddd15
fred <li1971@mcmaster.ca>
2022-11-07 10:51:57 -0500

    License Recognition Project Init

---
+++ main.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title(" Licence Recognition App ")
+window.geometry("600x400")
+newLabel = tk.Label(text=" This is the starting point of our project ")
+newLabel.grid(column=0, row=0)
+window.mainloop()
